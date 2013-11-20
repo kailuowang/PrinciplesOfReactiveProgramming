f6b9cf18479bea3c1de9a9385176315d9a418897
kailuo wang <kailuo.wang@gmail.com>
2013-11-19 22:28:46 -0500

    added blocking contruct for delay

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -52,7 +52,7 @@ package object nodescala {
     /** Returns a future with a unit value that is completed after time `t`.
      */
     def delay(t: Duration): Future[Unit] = Future {
-      Thread.sleep(t.toMillis)
+      blocking { Thread.sleep(t.toMillis) }
     }
 
     /** Completes this future with user input.

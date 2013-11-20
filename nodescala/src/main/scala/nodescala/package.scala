5866f64d39717050f18d6f6ed5fd998fe4ef6c2e
kailuo wang <kailuo.wang@gmail.com>
2013-11-19 22:33:27 -0500

    uses Await.ready instead of Thread.sleep

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -51,8 +51,14 @@ package object nodescala {
 
     /** Returns a future with a unit value that is completed after time `t`.
      */
-    def delay(t: Duration): Future[Unit] = Future {
-      blocking { Thread.sleep(t.toMillis) }
+    def delay(t: Duration): Future[Unit] = {
+      val p = Promise[Unit]
+      try{
+        Await.ready(Future.never, t)
+      }catch {
+        case t: TimeoutException => p.success(Unit)
+      }
+      p.future
     }
 
     /** Completes this future with user input.

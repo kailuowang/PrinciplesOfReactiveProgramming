431af0c31ff8d5c7d90b4ac466ddcdeb0b152142
kailuo wang <kailuo.wang@gmail.com>
2013-11-29 11:18:43 -0500

    implemented Observable for button and textfield

--- suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -9,7 +9,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
-import rx.lang.scala.Observable
+import rx.lang.scala._
+import rx.lang.scala.subscriptions.Subscription
 
 /** Basic facilities for dealing with Swing-like components.
 *
@@ -51,8 +52,13 @@ trait SwingApi {
       * @param field the text field
       * @return an observable with a stream of text field updates
       */
-    def textValues: Observable[String] = ???
-
+    def textValues: Observable[String] = Observable ( observer => {
+        field.subscribe {
+          case ValueChanged(fd) => observer.onNext(fd.text)
+        }
+        Subscription {}
+      }
+    )
   }
 
   implicit class ButtonOps(button: Button) {
@@ -62,7 +68,12 @@ trait SwingApi {
      * @param field the button
      * @return an observable with a stream of buttons that have been clicked
      */
-    def clicks: Observable[Button] = ???
+    def clicks: Observable[Button] = Observable { observer =>
+      button.subscribe {
+        case ButtonClicked(btn) => observer.onNext(btn)
+      }
+      Subscription {}
+    }
 
   }
 
